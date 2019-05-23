b0ba7fdab5e382e0df9d69e48aa7b0362a6e80d2
ashish.kumar <ashish.kumar@decimal.co.in>
2019-05-23 15:15:28 +0530

    Write wrapper object

--- src/main/java/decimal/apigateway/controller/RegistrationController.java
+++ src/main/java/decimal/apigateway/controller/RegistrationController.java
@@ -10,9 +10,8 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("engine/v1/")
-@CrossOrigin(exposedHeaders = { "custom_headers", "security-version", "hash", "authorization", "deviceid", "appid", "clientid", "deviceid", "nounce", "platform", "requestid", "requesttype", "servicename", "txnkey", "username", "content-type", "isforcelogin" , "auth_scheme"})
-public class RegistrationController
-{
+@CrossOrigin(exposedHeaders = {"custom_headers", "security-version", "hash", "authorization", "deviceid", "appid", "clientid", "deviceid", "nounce", "platform", "requestid", "requesttype", "servicename", "txnkey", "username", "content-type", "isforcelogin", "auth_scheme"})
+public class RegistrationController {
     private RegistrationService registrationService;
 
     @Autowired
@@ -22,7 +21,13 @@ public class RegistrationController
 
     @PostMapping("register")
     public Object executeService(@RequestBody String request, @RequestHeader Map<String, String> httpHeaders, HttpServletResponse response) throws IOException {
-        return registrationService.register(request, httpHeaders, response);
+
+        String serviceName = httpHeaders.get("servicename");
+
+        if (serviceName.contains("AUTH") || serviceName.contains("auth"))
+            return registrationService.authenticate(request, httpHeaders, response);
+        else
+            return registrationService.register(request, httpHeaders, response);
     }
 
 
