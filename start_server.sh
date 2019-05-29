82069b19435e7b86c7c825eb94a4dcce99668c89
charvee.punia <charvee.punia@decimal.co.in>
2019-05-29 19:06:46 +0530

    added changes

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 #hmod a+rx /opt/deployment/tpg-vconnect-config/${1}
-/usr/bin/nohup java -jar /opt/deployment/${PROJECT_NAME}/${JAR} --spring.config.location=file:${PROJECT_HOME}/application.properties > console.log &
+#/usr/bin/nohup java -jar /opt/deployment/${PROJECT_NAME}/${JAR} --spring.config.location=file:${PROJECT_HOME}/application.properties > ${PROJECT_HOME}/console.log &
+
+java -jar /opt/deployment/${PROJECT_NAME}/${JAR} --spring.config.location=file:${PROJECT_HOME}/application.properties
