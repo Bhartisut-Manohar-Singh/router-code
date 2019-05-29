18507219ada7da33d24fa1f1ba3ff9bf3c482006
charvee.punia <charvee.punia@decimal.co.in>
2019-05-29 18:01:32 +0530

    added permission

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #hmod a+rx /opt/deployment/tpg-vconnect-config/${1}
-java -jar /opt/deployment/${PROJECT_NAME}/${JAR}
+/usr/bin/nohup java -jar /opt/deployment/${PROJECT_NAME}/${JAR} --spring.config.location=file:${PROJECT_HOME}/application.properties > console.log &
