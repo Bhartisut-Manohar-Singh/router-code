c15867aca93a67bea4af66f15d3b96caea63a372
charvee.punia <charvee.punia@decimal.co.in>
2019-05-30 20:02:32 +0530

    added changes

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-/usr/bin/nohup java -jar /opt/deployment/${PROJECT_NAME}/${JAR} --spring.config.location=file:/usr/deployment/application.properties > /usr/depolyment/console.log &
+/usr/bin/nohup java -jar /opt/deployment/${PROJECT_NAME}/${JAR} --spring.config.location=file:/usr/deployment/application.properties 
 
 #java -jar /opt/deployment/tpg-api-gateway/${JAR} 
