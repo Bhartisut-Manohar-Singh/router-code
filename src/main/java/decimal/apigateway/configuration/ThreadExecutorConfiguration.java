9477647b47bea4f749141c90d6a9a07ca51f1821
ashish.kumar <ashish.kumar@decimal.co.in>
2019-06-04 12:48:25 +0530

    Optimize Async task executions

--- src/main/java/decimal/apigateway/configuration/ThreadExecutorConfiguration.java
+++ src/main/java/decimal/apigateway/configuration/ThreadExecutorConfiguration.java
@@ -1,5 +1,6 @@
 package decimal.apigateway.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -8,14 +9,26 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class ThreadExecutorConfiguration {
 
+    @Value("${timeToLive}")
+    int timeToLive;
+
+    @Value("${corePoolSize}")
+    int corePoolSize;
+
+    @Value("${maxPoolSize}")
+    int maxPoolSize;
+
+    @Value("${queueCapacity}")
+    int queueCapacity;
+
     @Bean(name = "myTaskExecutor")
     public TaskExecutor myTaskExecutor()
     {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(20);
-        executor.setQueueCapacity(20);
-        executor.setKeepAliveSeconds(20);
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setKeepAliveSeconds(timeToLive);
         executor.setAllowCoreThreadTimeOut(true);
 
         return executor;
