22d9de4bffe35cd11801c57be40dc6647003b9da
Shivam Mittal <shivam.mittal@decimal.co.in>
2019-12-23 13:07:29 +0530

    https

--- start_server.sh
+++ start_server.sh
@@ -3,6 +3,9 @@
 /usr/bin/nohup java -jar /opt/deployment/${PROJECT_NAME}/${JAR}
 -Djavax.net.ssl.keyStore=/etc/pki/devvahanacloud.jks
 -Djavax.net.ssl.keyStoreType=jks
+-Djavax.net.debug=ssl # very verbose debug
+-Djavax.net.ssl.keyStorePassword=admin@123
+
 --spring.config.location=file:/usr/deployment/application.properties
 
 
