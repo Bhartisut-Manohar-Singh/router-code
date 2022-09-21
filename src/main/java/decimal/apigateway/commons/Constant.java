7ef774ff92a4e248fe774394fd360e1d96995744
Mayank Goyal <mayank.goyal@decimal.co.in>
2022-09-21 23:08:18 +0530

    Changes For New Security Level

--- src/main/java/decimal/apigateway/commons/Constant.java
+++ src/main/java/decimal/apigateway/commons/Constant.java
@@ -21,7 +21,7 @@ public class Constant {
     public static final String INVALID_SERVICE_NAME = "Invalid Service Name";
     public static final String MULTIPART = "MULTIPART";
     public static final String JSON = "JSON";
-    public static final String IS_PAYLOAD_ENCRYPTED = "isPayloadEncrypted";
+    public static final String IS_PAYLOAD_ENCRYPTED = "ispayloadencrypted";
 
 
     public static final String ROUTER_HEADER_SECURITY_VERSION = "security-version";
