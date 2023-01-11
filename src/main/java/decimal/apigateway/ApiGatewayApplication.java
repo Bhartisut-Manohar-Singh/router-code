bfd6678ff53919375de3fe7d9a394cb888ed1a50
decimal <you@example.com>
2023-01-11 18:01:23 +0530

    Changes

--- src/main/java/decimal/apigateway/ApiGatewayApplication.java
+++ src/main/java/decimal/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package decimal.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -10,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 @EnableAspectJAutoProxy
 @EnableAsync
