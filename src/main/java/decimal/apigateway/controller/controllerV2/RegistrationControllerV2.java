58fbb02f9a9f33823e59748f59879b40d6bf7cd0
ashish.rathore <ashish.rathore@decimal.co.in>
2023-02-15 17:21:00 +0530

    removed System.out.println and used log.info

---
+++ src/main/java/decimal/apigateway/controller/controllerV2/RegistrationControllerV2.java
@@ -0,0 +1,75 @@
+package decimal.apigateway.controller.controllerV2;
+
+
+import decimal.apigateway.commons.Constant;
+import decimal.apigateway.exception.RouterException;
+import decimal.apigateway.model.MicroserviceResponse;
+import decimal.apigateway.service.RegistrationService;
+import decimal.apigateway.service.RegistrationServiceV2;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping("engine/v1/")
+@CrossOrigin(exposedHeaders = {"custom_headers", "security-version", "hash", "authorization", "deviceid", "sourceAppId", "clientid", "deviceid", "nounce", "platform", "requestid", "requesttype", "servicename", "txnkey", "username", "content-type", "isforcelogin", "auth_scheme","storageid"})
+@Log
+public class RegistrationControllerV2 {
+
+    private RegistrationServiceV2 registrationServiceV2;
+
+    @Autowired
+    public RegistrationControllerV2(RegistrationServiceV2 registrationServiceV2) {
+        this.registrationServiceV2 = registrationServiceV2;
+    }
+
+    @PostMapping("register")
+    public Object executeService(@RequestBody String request, @RequestHeader Map<String, String> httpHeaders, HttpServletResponse response) throws IOException, RouterException {
+
+        String serviceName = httpHeaders.get("servicename");
+        log.info("Service Name: " + serviceName);
+
+        log.info("====================Call for register=============================");
+        if (serviceName.contains("AUTH") || serviceName.contains("auth"))
+            return registrationServiceV2.authenticate(request, httpHeaders, response);
+        else if(serviceName.equalsIgnoreCase("REGISTERAPP"))
+            return registrationServiceV2.register(request, httpHeaders, response);
+        else
+        {
+            String message = Constant.INVALID_SERVICE_NAME;
+            String status = Constant.FAILURE_STATUS;
+            MicroserviceResponse microserviceResponse = new MicroserviceResponse(status, message, "");
+
+            throw new RouterException(Constant.ROUTER_SERVICE_NOT_FOUND,  Constant.ROUTER_ERROR_TYPE_VALIDATION,microserviceResponse);
+
+        }
+
+        // return new ResponseEntity<>( Constant.ROUTER_SERVICE_NOT_FOUND
+//, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @PostMapping("authenticate")
+    public Object authenticate(@RequestBody String request, @RequestHeader Map<String, String> httpHeaders, HttpServletResponse response) throws IOException, RouterException {
+        log.info("====================Call for authenticate=============================");
+        return registrationServiceV2.authenticate(request, httpHeaders, response);
+    }
+
+    @PostMapping("logout")
+    public Object logout(@RequestBody String request, @RequestHeader Map<String, String> httpHeaders, HttpServletResponse response) throws IOException, RouterException {
+        log.info("====================Call for logout=============================");
+        return registrationServiceV2.logout(request, httpHeaders, response);
+    }
+
+    @PostMapping("forceLogout")
+    public Object forceLogout(@RequestBody String request, @RequestHeader Map<String, String> httpHeaders, HttpServletResponse response) throws IOException, RouterException {
+        log.info("====================Call for force logout=============================");
+
+        return registrationServiceV2.forceLogout(request, httpHeaders, response);
+    }
+
+}
---
+++ src/main/java/decimal/apigateway/service/executionImplV2/RegistrationServiceImplV2.java
@@ -0,0 +1,246 @@
+package decimal.apigateway.service.executionImplV2;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import decimal.apigateway.commons.Constant;
+import decimal.apigateway.commons.ResponseOperations;
+import decimal.apigateway.exception.RouterException;
+import decimal.apigateway.model.MicroserviceResponse;
+import decimal.apigateway.service.LogsWriter;
+import decimal.apigateway.service.RegistrationServiceV2;
+import decimal.apigateway.service.clients.AuthenticationClient;
+import decimal.apigateway.service.clients.SecurityClient;
+import decimal.apigateway.service.validator.RequestValidator;
+import decimal.apigateway.service.validator.RequestValidatorV2;
+import decimal.logs.filters.AuditTraceFilter;
+import decimal.logs.masking.JsonMasker;
+import decimal.logs.model.AuditPayload;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+import static decimal.apigateway.commons.Constant.JSON;
+
+
+@Service
+@Log
+public class RegistrationServiceImplV2 implements RegistrationServiceV2 {
+
+    private SecurityClient securityClient;
+
+    private AuthenticationClient authenticationClient;
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    ResponseOperations responseOperations;
+
+    @Autowired
+    RequestValidatorV2 requestValidatorV2;
+
+    @Autowired
+    AuditTraceFilter auditTraceFilter;
+
+    @Autowired
+    LogsWriter logsWriter;
+
+    @Autowired
+    AuditPayload auditPayload;
+
+    @Value("${isHttpTracingEnabled}")
+    boolean isHttpTracingEnabled;
+
+    @Autowired
+    public RegistrationServiceImplV2(SecurityClient securityClient, AuthenticationClient authenticationClient, ObjectMapper objectMapper) {
+        this.securityClient = securityClient;
+        this.authenticationClient = authenticationClient;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Object register(String request, Map<String, String> httpHeaders, HttpServletResponse response) throws IOException, RouterException {
+
+        auditPayload = logsWriter.initializeLog(request,JSON, httpHeaders);
+
+        auditTraceFilter.setIsServicesLogsEnabled(isHttpTracingEnabled);
+
+        ObjectNode jsonNodes = objectMapper.convertValue(requestValidatorV2.validateRegistrationRequest(request, httpHeaders), ObjectNode.class);
+
+        String userName = jsonNodes.get("username").asText();
+
+        httpHeaders.put("username", userName);
+
+        auditPayload.getRequest().setRequestBody(request);
+
+        httpHeaders.put("executionsource","API-GATEWAY");
+
+        ResponseEntity<Object> responseEntity = authenticationClient.register(request, httpHeaders);
+
+        HttpHeaders responseHeaders = responseEntity.getHeaders();
+        if(responseHeaders!=null && responseHeaders.containsKey("status"))
+            auditPayload.setStatus(responseHeaders.get("status").get(0));
+
+        MicroserviceResponse registerResponse = objectMapper.convertValue(responseEntity.getBody(),MicroserviceResponse.class);
+        Map<String, Object> rsaKeysMap = objectMapper.convertValue(registerResponse.getResponse(), new TypeReference<Map<String, Object>>() {
+        });
+
+        auditPayload.getResponse().setResponse(objectMapper.writeValueAsString(registerResponse.getResponse()));
+
+        String jwtToken = String.valueOf(rsaKeysMap.get("jwtToken"));
+
+        ObjectNode node = objectMapper.createObjectNode();
+
+        response.addHeader("Authorization", "Bearer " + jwtToken);
+
+        node.put("Authorization", "Bearer " + jwtToken);
+
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("rsa", objectMapper.writeValueAsString(rsaKeysMap));
+
+        String finalResponse = responseOperations.prepareResponseObject(httpHeaders.get("requestid"),
+                httpHeaders.get("servicename"),
+                objectMapper.writeValueAsString(responseMap)).toString();
+
+        MicroserviceResponse responseHash = securityClient.generateResponseHash(finalResponse, httpHeaders);
+
+        response.addHeader("hash", responseHash.getMessage());
+        node.put("hash", responseHash.getMessage());
+
+        auditPayload.getResponse().setStatus(String.valueOf(HttpStatus.OK.value()));
+
+        logsWriter.updateLog(auditPayload);
+        return finalResponse;
+    }
+
+    @Override
+    public Object  authenticate(String request, Map<String, String> httpHeaders, HttpServletResponse response) throws IOException, RouterException {
+
+        auditPayload = logsWriter.initializeLog(request,JSON, httpHeaders);
+
+        MicroserviceResponse microserviceResponse = requestValidatorV2.validateAuthentication(request, httpHeaders);
+
+        httpHeaders.put("username", microserviceResponse.getMessage());
+
+        Map<String, String> customData = microserviceResponse.getCustomData();
+
+        if(customData != null)
+        {
+            String logsRequired = customData.get("appLogs");
+            String serviceLog = customData.get("serviceLog");
+            String logPurgeDays =  customData.get("logpurgedays");
+            auditTraceFilter.setPurgeDays(logPurgeDays);
+            httpHeaders.put(Constant.KEYS_TO_MASK, customData.get(Constant.KEYS_TO_MASK));
+            httpHeaders.put("logsrequired", logsRequired);
+            httpHeaders.put("servicelogs", serviceLog);
+            httpHeaders.put("logpurgedays",logPurgeDays);
+            auditTraceFilter.setIsServicesLogsEnabled(isHttpTracingEnabled && "Y".equalsIgnoreCase(logsRequired) && "Y".equalsIgnoreCase(serviceLog));
+
+        }
+        else
+            auditTraceFilter.setIsServicesLogsEnabled(isHttpTracingEnabled);
+
+        String keysToMask = httpHeaders.get(Constant.KEYS_TO_MASK);
+
+        List<String> maskKeys = new ArrayList<>();
+
+        if (keysToMask != null && !keysToMask.isEmpty()) {
+            String[] keysToMaskArr = keysToMask.split(",");
+            maskKeys = Arrays.asList(keysToMaskArr);
+        }
+
+        Object plainRequest = microserviceResponse.getResponse();
+
+        ObjectNode nodes = objectMapper.createObjectNode();
+
+        String requestBody = JsonMasker.maskMessage(plainRequest.toString(), maskKeys);
+        auditPayload.getRequest().setRequestBody(requestBody);
+
+        auditPayload.getRequestIdentifier().setLoginId(microserviceResponse.getMessage().split(Constant.TILD_SPLITTER)[2]);
+
+        httpHeaders.put("executionsource","API-GATEWAY");
+
+        ResponseEntity<Object> responseEntity = authenticationClient.authenticate(plainRequest, httpHeaders);
+
+        HttpHeaders responseHeaders = responseEntity.getHeaders();
+        if(responseHeaders!=null && responseHeaders.containsKey("status"))
+            auditPayload.setStatus(responseHeaders.get("status").get(0));
+
+        MicroserviceResponse authenticateResponse = objectMapper.convertValue(responseEntity.getBody(),MicroserviceResponse.class);
+
+        Map<String, Object> authResponse = objectMapper.convertValue(authenticateResponse.getResponse(), new TypeReference<Map<String, Object>>() {
+        });
+
+        Object finalResponse = responseOperations.prepareResponseObject(httpHeaders.get("requestid"),
+                httpHeaders.get("servicename"),
+                objectMapper.writeValueAsString(authResponse));
+
+        String maskedResponse = JsonMasker.maskMessage(finalResponse.toString(), maskKeys);
+        auditPayload.getResponse().setResponse(maskedResponse);
+
+        MicroserviceResponse encryptedResponse = securityClient.encryptResponse(finalResponse, httpHeaders);
+
+        if (!encryptedResponse.getStatus().equalsIgnoreCase(Constant.SUCCESS_STATUS)) {
+            return new ResponseEntity<>(authenticateResponse.getResponse(), HttpStatus.BAD_REQUEST);
+        }
+
+        ObjectNode node = objectMapper.createObjectNode();
+
+        node.put("Authorization", authResponse.get("jwtToken").toString());
+        response.addHeader("Authorization", authResponse.get("jwtToken").toString());
+
+
+
+        Map<String, String> finalResponseMap = new HashMap<>();
+        finalResponseMap.put("response", encryptedResponse.getMessage());
+
+        MicroserviceResponse authResponseHash = securityClient.generateAuthResponseHash(finalResponse.toString(), httpHeaders);
+
+        response.addHeader("hash", authResponseHash.getMessage());
+
+        node.put("hash", authResponseHash.getMessage());
+
+        auditPayload.getResponse().setStatus(String.valueOf(HttpStatus.OK.value()));
+
+        logsWriter.updateLog(auditPayload);
+
+        return finalResponseMap;
+    }
+
+    @Override
+    public Object forceLogout(String request, Map<String, String> httpHeaders, HttpServletResponse response) throws RouterException {
+        httpHeaders.put("executionsource","API-GATEWAY");
+        ResponseEntity<Object> responseEntity = authenticationClient.forceLogout(httpHeaders);
+        MicroserviceResponse microserviceResponse = objectMapper.convertValue(responseEntity.getBody(),MicroserviceResponse.class);
+        return new ResponseEntity(microserviceResponse,responseEntity.getHeaders(),HttpStatus.OK);
+    }
+
+    @Override
+    public Object logout(String request, Map<String, String> httpHeaders, HttpServletResponse response) {
+        try {
+            MicroserviceResponse microserviceResponse = requestValidatorV2.validateLogout(request, httpHeaders);
+
+            httpHeaders.put("username", microserviceResponse.getResponse().toString());
+
+            auditTraceFilter.requestIdentifier.setLoginId(microserviceResponse.getResponse().toString().split(Constant.TILD_SPLITTER)[2]);
+
+        }
+        catch (Exception ex)
+        {
+            log.info("Security Failure!!!!!");
+        }
+
+        httpHeaders.put("executionsource","API-GATEWAY");
+        ResponseEntity<Object> responseEntity = authenticationClient.logout(httpHeaders);
+        MicroserviceResponse microserviceResponse = objectMapper.convertValue(responseEntity.getBody(),MicroserviceResponse.class);
+        return new ResponseEntity(microserviceResponse,responseEntity.getHeaders(),HttpStatus.OK);
+    }
+}
