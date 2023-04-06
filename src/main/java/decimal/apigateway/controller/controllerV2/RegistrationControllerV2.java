49ce9e959e05d6fe490d12d10635397182a5a930
sakshi <sakshi.yadav@decimal.co.in>
2023-04-06 16:54:19 +0530

    auth api v2

--- src/main/java/decimal/apigateway/controller/controllerV2/RegistrationControllerV2.java
+++ src/main/java/decimal/apigateway/controller/controllerV2/RegistrationControllerV2.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("engine/v2/")
-@CrossOrigin(exposedHeaders = {"custom_headers", "security-version", "hash", "authorization", "deviceid", "sourceAppId", "clientid", "deviceid", "nounce", "platform", "requestid", "requesttype", "servicename", "txnkey", "username", "content-type", "isforcelogin", "auth_scheme","storageid", "imeinumber"})
+//@CrossOrigin(exposedHeaders = {"custom_headers", "security-version", "hash", "authorization", "deviceid", "sourceAppId", "clientid", "deviceid", "nounce", "platform", "requestid", "requesttype", "servicename", "txnkey", "username", "content-type", "isforcelogin", "auth_scheme","storageid", "imeinumber"})
 @Log
 public class RegistrationControllerV2 {
 
