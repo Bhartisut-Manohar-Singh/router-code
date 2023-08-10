d4a28c3bc8d79a360142003cdae7768203d2e4e9
somyasharma1103 <somya.sharma@decimal.co.in>
2023-08-10 11:42:19 +0530

    java11Changes

--- src/main/java/decimal/apigateway/ApiGatewayApplication.java
+++ src/main/java/decimal/apigateway/ApiGatewayApplication.java
@@ -1,6 +1,7 @@
 package decimal.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -15,6 +16,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAspectJAutoProxy
 @EnableAsync
 @EnableSwagger2
+@EnableAutoConfiguration(exclude = {
+        org.springframework.cloud.client.serviceregistry.ServiceRegistryAutoConfiguration.class,
+        org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationAutoConfiguration.class
+})
 @ComponentScan(basePackages = { "decimal.apigateway","decimal.logs.kafka"," decimal.logs.connector"})
 public class ApiGatewayApplication {
 
