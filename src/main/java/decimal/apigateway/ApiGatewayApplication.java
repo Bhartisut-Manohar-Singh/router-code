f0852c682d204212f9d508d03526d24bdfcce0cc
Mayank Goyal <mayank.goyal@decimal.co.in>
2023-08-11 13:03:14 +0530

    Excluding Auto Configuration for Service Registry

--- src/main/java/decimal/apigateway/ApiGatewayApplication.java
+++ src/main/java/decimal/apigateway/ApiGatewayApplication.java
@@ -1,7 +1,8 @@
 package decimal.apigateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationAutoConfiguration;
+import org.springframework.cloud.client.serviceregistry.ServiceRegistryAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,16 +11,12 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ServiceRegistryAutoConfiguration.class, AutoServiceRegistrationAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableAspectJAutoProxy
 @EnableAsync
 @EnableSwagger2
-@EnableAutoConfiguration(exclude = {
-        org.springframework.cloud.client.serviceregistry.ServiceRegistryAutoConfiguration.class,
-        org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationAutoConfiguration.class
-})
 @ComponentScan(basePackages = { "decimal.apigateway","decimal.logs.kafka"," decimal.logs.connector"})
 public class ApiGatewayApplication {
 
