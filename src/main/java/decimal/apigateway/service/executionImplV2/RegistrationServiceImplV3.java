70cbf3bee2e81480bfd372eff33768d035512600
ashish.rathore@decimal.co.in <ashish.rathore@decimal.co.in>
2023-10-25 17:18:00 +0530

    exception changes in ExceptionController.java

--- src/main/java/decimal/apigateway/service/executionImplV2/RegistrationServiceImplV3.java
+++ src/main/java/decimal/apigateway/service/executionImplV2/RegistrationServiceImplV3.java
@@ -97,7 +97,7 @@ public class RegistrationServiceImplV3 implements RegistrationServiceV3 {
             //ResponseEntity<Object> responseEntity = authenticationClient.publicRegister(request, httpHeaders);
             ResponseEntity<Object> responseEntity = authenticationService.publicRegister(request, httpHeaders);
 
-            log.info("Response from Step 1....." + responseEntity.toString());
+            log.info("Response from Step 1....." + objectMapper.writeValueAsString(responseEntity));
 
             HttpHeaders responseHeaders = responseEntity.getHeaders();
             if (responseHeaders != null && responseHeaders.containsKey("status"))
