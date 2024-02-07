b9dab6734506bc269ead1456b70edcd672ac8925
nishant.sharma <nishant.sharma@decimal.co.in>
2024-02-07 11:49:25 +0530

    new changes

--- src/main/java/decimal/apigateway/enums/Headers.java
+++
@@ -1,7 +0,0 @@
-package decimal.apigateway.enums;
-
-public enum Headers {
-
-    apiName, requestid, orgid, appid, logorgid, logappid, username, password, deviceid, loginid, servicename, nounce,clientsecret,txnkey,hash, sourceappid, destinationappid, sourceorgid, destinationorgid, auth_scheme
-
-}
