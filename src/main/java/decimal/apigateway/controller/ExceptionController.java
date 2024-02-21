444d3e0e7504c4aa90ea3191eb9fb5e5045b7f76
Lipika Barapatre <lipika.b@decimal.co.in>
2024-02-21 16:51:25 +0530

    code cleanup

--- src/main/java/decimal/apigateway/controller/ExceptionController.java
+++ src/main/java/decimal/apigateway/controller/ExceptionController.java
@@ -215,12 +215,6 @@ public class ExceptionController {
         return new ResponseEntity<>(errorResponse, responseHeaders,HttpStatus.BAD_REQUEST);
     }*/
 
-//    @ExceptionHandler(value = RequestNotPermitted.class)
-//    public ResponseEntity<Object> handleRouterException(RequestNotPermitted ex) throws JsonProcessingException {
-//        log.info("Inside request not permission exception handler - " + ex.getMessage());
-//
-//        return new ResponseEntity<>(ex.getMessage(), null, HttpStatus.TOO_MANY_REQUESTS);
-//    }
 
     @ExceptionHandler(value = PublicTokenCreationException.class)
     public ResponseEntity<Object> handlePublicJwtCreationException(PublicTokenCreationException ex) {
