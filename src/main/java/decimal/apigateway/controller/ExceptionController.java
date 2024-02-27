87c9784960877694d8790652f9d5d778d5a480ba
Lipika Barapatre <lipika.b@decimal.co.in>
2024-02-27 13:40:39 +0530

    changes

--- src/main/java/decimal/apigateway/controller/ExceptionController.java
+++ src/main/java/decimal/apigateway/controller/ExceptionController.java
@@ -216,12 +216,12 @@ public class ExceptionController {
         return new ResponseEntity<>(errorResponse, responseHeaders,HttpStatus.BAD_REQUEST);
     }*/
 
-    @ExceptionHandler(value = RequestNotPermitted.class)
-    public ResponseEntity<Object> handleRouterException(RequestNotPermitted ex) throws JsonProcessingException {
-        log.info("Inside request not permission exception handler - " + ex.getMessage());
-
-        return new ResponseEntity<>(ex.getMessage(), null, HttpStatus.TOO_MANY_REQUESTS);
-    }
+//    @ExceptionHandler(value = RequestNotPermitted.class)
+//    public ResponseEntity<Object> handleRouterException(RequestNotPermitted ex) throws JsonProcessingException {
+//        log.info("Inside request not permission exception handler - " + ex.getMessage());
+//
+//        return new ResponseEntity<>(ex.getMessage(), null, HttpStatus.TOO_MANY_REQUESTS);
+//    }
 
 
 
--- src/main/java/decimal/apigateway/service/rateLimiter/RateLimitService.java
+++ src/main/java/decimal/apigateway/service/rateLimiter/RateLimitService.java
@@ -1,6 +1,7 @@
 package decimal.apigateway.service.rateLimiter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import decimal.apigateway.commons.RouterResponseCode;
 import decimal.apigateway.entity.*;
 import decimal.apigateway.exception.RequestNotPermitted;
@@ -12,11 +13,13 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
 
@@ -45,40 +48,56 @@ public class RateLimitService {
 
 
     public Boolean allowRequest(String appId, String serviceName, Map<String, String> httpHeaders) throws RouterException, IOException {
-        auditPayload = logsWriter.initializeLog("request", JSON, httpHeaders);
+        try {
+            auditPayload = logsWriter.initializeLog("request", JSON, httpHeaders);
 
-        // checks in redis if rate limiting config is present
-        Optional<RateLimitConfig> rateLimitAppConfig = rateLimitRepo.findById(appId);
+            // checks in redis if rate limiting config is present
+            Optional<RateLimitConfig> rateLimitAppConfig = rateLimitRepo.findById(appId);
 
-        if (rateLimitAppConfig.isPresent()) {
-            log.info("------- going to consume token for app ---------");
-                if (!consumeTokens(rateLimitAppConfig.get(),"rl~"+appId)) {
+            if (rateLimitAppConfig.isPresent()) {
+                log.info("------- going to consume token for app ---------");
+                if (!consumeTokens(rateLimitAppConfig.get(), "rl~" + appId)) {
                     throw new RequestNotPermitted("No tokens left for this app. Please try again later.");
                 }
-
             }
 
-        Optional<RateLimitConfig> rateLimitServiceConfig = rateLimitRepo.findById(appId + "~" + serviceName);
+            Optional<RateLimitConfig> rateLimitServiceConfig = rateLimitRepo.findById(appId + "~" + serviceName);
 
-        if(rateLimitServiceConfig.isPresent()){
-            log.info("------- going to consume token for service ---------");
-            if (!consumeTokens(rateLimitServiceConfig.get(),"rl~"+appId+"~"+serviceName)) {
-                throw new RequestNotPermitted("No tokens left for this service. Please try again later.");
+            if (rateLimitServiceConfig.isPresent()) {
+                log.info("------- going to consume token for service ---------");
+                if (!consumeTokens(rateLimitServiceConfig.get(), "rl~" + appId + "~" + serviceName)) {
+                    throw new RequestNotPermitted("No tokens left for this service. Please try again later.");
+                }
             }
-        }
+
             // Both app and service checks passed
             return true;
+
+        } catch (RequestNotPermitted e) {
+            auditPayload.getResponse().setResponse(new Gson().toJson("Too many requests. Please try again later."));
+            auditPayload.getResponse().setStatus(String.valueOf(HttpStatus.TOO_MANY_REQUESTS.value()));
+            auditPayload.getResponse().setTimestamp(Instant.now());
+
+            logsWriter.updateLog(auditPayload);
+            return false;
+
+        } catch (Exception e) {
+            log.info("An error occurred: " + e.getMessage());
+            throw new RouterException("An error occurred while processing the request.", e);
+
         }
+    }
 
 
 
     boolean consumeTokens(RateLimitConfig rateLimitConfig, String key){
         log.info("------ inside consume tokens---------");
-//        if(!redisTemplate.hasKey(key)){
-//            valueOps.set(key,rateLimitConfig.getMaxAllowedHits(),rateLimitConfig.getDuration(),rateLimitConfig.getDurationUnit());
-//            log.info("-------created new config-------"+rateLimitConfig.getMaxAllowedHits()+"-------------"+rateLimitConfig.getDuration()+"-------------"+rateLimitConfig.getDurationUnit());
-//        }
-        valueOps.setIfAbsent(key,rateLimitConfig.getMaxAllowedHits(),rateLimitConfig.getDuration(),rateLimitConfig.getDurationUnit());
+        if(!redisTemplate.hasKey(key)){
+            valueOps.set(key,rateLimitConfig.getMaxAllowedHits(),rateLimitConfig.getDuration(),rateLimitConfig.getDurationUnit());
+            log.info("-------created new config-------"+rateLimitConfig.getMaxAllowedHits()+"-------------"+rateLimitConfig.getDuration()+"-------------"+rateLimitConfig.getDurationUnit());
+        }
+//        Boolean bool = valueOps.setIfAbsent(key,rateLimitConfig.getMaxAllowedHits(),rateLimitConfig.getDuration(),rateLimitConfig.getDurationUnit());
+//        log.info("-------- returned value after setting the key --------"+bool);
 
         Long newCtr = valueOps.decrement(key);
         log.info("--------- tokens left are ------- : "+newCtr);
