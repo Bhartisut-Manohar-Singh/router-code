e11fac0a932769b14ff1f18cb8d295d53a669ad2
Lipika Barapatre <lipika.b@decimal.co.in>
2024-02-28 14:12:56 +0530

    changes

--- src/main/java/decimal/apigateway/exception/RateLimitError.java
+++ src/main/java/decimal/apigateway/exception/RateLimitError.java
@@ -1,11 +1,15 @@
 package decimal.apigateway.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class RateLimitError {
     String message;
     String status;
