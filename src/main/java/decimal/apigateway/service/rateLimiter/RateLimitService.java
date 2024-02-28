7fcc9c51f56fffb19f49e0a2b17b1c7d66d42eed
Mayank Goyal <mayank.goyal@decimal.co.in>
2024-02-28 22:59:10 +0530

    Changes for logs

--- src/main/java/decimal/apigateway/service/rateLimiter/RateLimitService.java
+++ src/main/java/decimal/apigateway/service/rateLimiter/RateLimitService.java
@@ -1,24 +1,31 @@
 package decimal.apigateway.service.rateLimiter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import decimal.apigateway.commons.RouterResponseCode;
 import decimal.apigateway.entity.*;
+import decimal.apigateway.exception.RateLimitError;
 import decimal.apigateway.exception.RequestNotPermitted;
 import decimal.apigateway.exception.RouterException;
 import decimal.apigateway.repository.RateLimitRepo;
 import decimal.apigateway.service.LogsWriter;
+import decimal.logs.filters.AuditTraceFilter;
 import decimal.logs.model.AuditPayload;
+import decimal.logs.model.Request;
+import decimal.logs.model.Response;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
 
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
 
@@ -33,6 +40,18 @@ public class RateLimitService {
     @Autowired
     private RedisTemplate<String, Long> redisTemplate;
 
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    AuditPayload auditPayload;
+
+    @Autowired
+    LogsWriter logsWriter;
+
+    @Autowired
+    AuditTraceFilter auditTraceFilter;
+
     private ValueOperations<String, Long> valueOps;
 
     @PostConstruct
@@ -41,15 +60,22 @@ public class RateLimitService {
     }
 
 
-    public Boolean allowRequest(String appId, String serviceName, Map<String, String> httpHeaders) throws RouterException, IOException {
+    public Object allowRequest(String appId, String serviceName, Map<String, String> httpHeaders) throws IOException {
 
-        // checks in redis if rate limiting config is present
+        AuditPayload auditPayload = new AuditPayload();
+        auditPayload.setRequest(new Request());
+        auditPayload.setResponse(new Response());
+
+        auditPayload = logsWriter.initializeLog("request", JSON,httpHeaders,serviceName,auditPayload);
+        auditTraceFilter.setIsServicesLogsEnabled(true);
+
         Optional<RateLimitConfig> rateLimitAppConfig = rateLimitRepo.findById(appId);
 
         if (rateLimitAppConfig.isPresent()) {
             log.info("------- going to consume token for app ---------");
                 if (!consumeTokens(rateLimitAppConfig.get(),"RL~"+appId)) {
-                 throw new RequestNotPermitted("No tokens left for this app. Please try again later.");
+                 //throw new RequestNotPermitted("No tokens left for this app. Please try again later.");
+                    return failureResponse(auditPayload);
                 }
 
             }
@@ -59,13 +85,25 @@ public class RateLimitService {
         if(rateLimitServiceConfig.isPresent()){
             log.info("------- going to consume token for service ---------");
             if (!consumeTokens(rateLimitServiceConfig.get(),"RL~"+appId+"~"+serviceName)) {
-                throw new RequestNotPermitted("No tokens left for this service. Please try again later.");
+              //  throw new RequestNotPermitted("No tokens left for this service. Please try again later.");
+                return failureResponse(auditPayload);
+
             }
         }
-            // Both app and service checks passed
             return true;
         }
 
+    private Object failureResponse(AuditPayload auditPayload) throws JsonProcessingException {
+        RateLimitError rateLimitError = new RateLimitError("Too many requests",FAILURE_STATUS,HttpStatus.TOO_MANY_REQUESTS.value());
+
+        auditPayload.getResponse().setResponse(mapper.writeValueAsString(rateLimitError));
+        auditPayload.getResponse().setStatus(FAILURE_STATUS);
+        auditPayload.getResponse().setTimestamp(Instant.now());
+        auditPayload.setStatus(FAILURE_STATUS);
+        logsWriter.updateLog(auditPayload);
+
+        return new ResponseEntity<>(rateLimitError, null, HttpStatus.TOO_MANY_REQUESTS);
+    }
 
 
     boolean consumeTokens(RateLimitConfig rateLimitConfig, String key){
