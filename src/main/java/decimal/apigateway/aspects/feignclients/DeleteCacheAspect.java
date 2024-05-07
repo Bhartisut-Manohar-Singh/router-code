b8c0ef17d4f5ef17905706a28607feb7d0e7b98d
ashish <ashish.rathore@decimal.co.in>
2024-05-07 20:23:13 +0530

    for merge request

--- src/main/java/decimal/apigateway/aspects/feignclients/DeleteCacheAspect.java
+++ src/main/java/decimal/apigateway/aspects/feignclients/DeleteCacheAspect.java
@@ -57,7 +57,7 @@ public class DeleteCacheAspect {
     }
 
 
-    @Before(value = "execution(* decimal.apigateway.controller.DeleteCacheController.*(..)) && args(appId, orgId , clientSecret)")
+    @Before(value = "execution(* decimal.apigateway.controller.DeleteSessionController.*(..)) && args(appId, orgId , clientSecret)")
     public void generateSSOToken(@RequestHeader String appId, @RequestHeader String orgId, @RequestHeader String clientSecret) throws ParseException {
         log.info("in Aspect method()");
         this.verifyClientSecret(appId,orgId, clientSecret);
